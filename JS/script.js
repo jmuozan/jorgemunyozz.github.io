889240a2feea4f46701e9d23e9f3d44e92e5d7a5
jmuozan <146674258+jmuozan@users.noreply.github.com>
2024-06-07 14:43:43 +0200

    transition animation

--- JS/script.js
+++ JS/script.js
@@ -9,6 +9,12 @@ document.addEventListener('DOMContentLoaded', function () {
         }, 600); // duration of the animation + cooldown period
       }
     });
+
+    // Add click event for transition effect
+    link.addEventListener('click', function (event) {
+      event.preventDefault();
+      triggerTransition(this.href);
+    });
   });
 
   // Apply hover animation to links in links-column
@@ -138,5 +144,30 @@ function immediateScroll(targetX) {
   window.scrollTo({
     left: targetX,
     behavior: 'smooth'
+  });
+}
+
+// Function to trigger the transition effect
+function triggerTransition(href) {
+  const transitionBlock = document.createElement('div');
+  transitionBlock.classList.add('transition-block');
+  document.body.appendChild(transitionBlock);
+
+  const transitionBlockBackground = document.createElement('div');
+  transitionBlockBackground.classList.add('transition-block-background');
+  document.body.appendChild(transitionBlockBackground);
+
+  // Trigger the transition
+  setTimeout(() => {
+    transitionBlock.classList.add('show');
+  }, 10); // Slight delay to ensure the transition is visible
+
+  // Add event listener for transition end
+  transitionBlock.addEventListener('transitionend', () => {
+    // Start the second transition
+    transitionBlockBackground.style.top = '0';
+    transitionBlockBackground.addEventListener('transitionend', () => {
+      window.location.href = href; // Redirect to the new page after transition
+    });
   });
 }
