3daf517086ff98ef4a3588d7c6d0e7aef41025e1
jmuozan <146674258+jmuozan@users.noreply.github.com>
2024-06-09 00:21:12 +0200

    changing format

--- JS/works.js
+++ JS/works.js
@@ -1,20 +1,16 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const links = document.querySelectorAll('.link-item');
+document.addEventListener('DOMContentLoaded', () => {
+    const contentItems = document.querySelectorAll('.content div');
     const hoverImage = document.getElementById('hover-image');
 
-    links.forEach(link => {
-        link.addEventListener('mouseover', function () {
-            const imageUrl = link.getAttribute('data-image');
-            hoverImage.src = imageUrl;
-            hoverImage.style.display = 'block';
-            hoverImage.style.opacity = '1'; // Fade in the image
+    contentItems.forEach(item => {
+        item.addEventListener('mouseenter', () => {
+            const imageUrl = item.getAttribute('data-image');
+            hoverImage.style.backgroundImage = `url(${imageUrl})`;
+            hoverImage.classList.add('visible');
         });
 
-        link.addEventListener('mouseout', function () {
-            hoverImage.style.opacity = '0'; // Fade out the image
-            setTimeout(() => {
-                hoverImage.style.display = 'none';
-            }, 500); // Match the duration of the transition
+        item.addEventListener('mouseleave', () => {
+            hoverImage.classList.remove('visible');
         });
     });
 });
