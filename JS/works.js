165a25b983977951cf2ecd91c9491247aca0a935
jmuozan <146674258+jmuozan@users.noreply.github.com>
2024-06-11 19:10:01 +0200

    Text when hover

--- JS/works.js
+++ JS/works.js
@@ -1,15 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
     const contentItems = document.querySelectorAll('.content-item div');
+    const dynamicParagraph = document.querySelector('.dynamic-paragraph');
+
+    const paragraphs = {
+        "REENCUENTRO": "Description for REENCUENTRO",
+        "AI.RTISANSHIP": "Description for AI.RTISANSHIP",
+        "SLOW FURNITURE": "Description for SLOW FURNITURE",
+        "HAPPY MEAL OF THE FUTURE": "Description for HAPPY MEAL OF THE FUTURE",
+        "GRESAL": "Description for GRESAL",
+        "TALENT-HOP": "Description for TALENT-HOP"
+    };
 
     contentItems.forEach(item => {
         item.addEventListener('mouseenter', () => {
             const imageUrl = item.getAttribute('data-image');
+            const itemText = item.textContent.trim();
             document.body.style.setProperty('--bg-image', `url(${imageUrl})`);
             document.body.classList.add('animate-bg');
+            dynamicParagraph.textContent = paragraphs[itemText] || "";
         });
 
         item.addEventListener('mouseleave', () => {
             document.body.classList.remove('animate-bg');
+            dynamicParagraph.textContent = "";
         });
     });
 });
