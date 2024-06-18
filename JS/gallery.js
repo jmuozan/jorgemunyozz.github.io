617458a19678b5cf34788f82cba9a3e8fa44eca5
jmuozan <146674258+jmuozan@users.noreply.github.com>
2024-06-18 17:31:37 +0200

    GALLERY INFINITE LOOP

--- JS/gallery.js
+++ JS/gallery.js
@@ -1,13 +1,25 @@
-document.addEventListener('wheel', (event) => {
-    if (event.deltaY > 0) {
-        document.querySelector('.image-container').scrollBy({
-            left: 100,
+document.addEventListener('DOMContentLoaded', () => {
+    const container = document.getElementById('container');
+    const imageContainer = document.getElementById('image-container');
+    const images = imageContainer.querySelectorAll('img');
+    const totalImages = images.length;
+    const halfLength = totalImages / 6;
+
+    container.scrollLeft = imageContainer.scrollWidth / 4;
+
+    container.addEventListener('wheel', (event) => {
+        event.preventDefault();
+        container.scrollBy({
+            left: event.deltaY * 4, // Adjust the multiplier for faster/slower scroll
             behavior: 'smooth'
         });
-    } else {
-        document.querySelector('.image-container').scrollBy({
-            left: -100,
-            behavior: 'smooth'
-        });
-    }
+    });
+
+    container.addEventListener('scroll', () => {
+        if (container.scrollLeft >= imageContainer.scrollWidth / 2) {
+            container.scrollLeft = container.scrollLeft - (imageContainer.scrollWidth / 2);
+        } else if (container.scrollLeft <= 0) {
+            container.scrollLeft = container.scrollLeft + (imageContainer.scrollWidth / 2);
+        }
+    });
 });
