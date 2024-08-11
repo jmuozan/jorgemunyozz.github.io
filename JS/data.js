7361772669af763e6ba45842f0530512f748df84
jmuozan <146674258+jmuozan@users.noreply.github.com>
2024-08-11 20:28:28 +0200

    CANVIS

---
+++ JS/data.js
@@ -0,0 +1,113 @@
+const data = [
+    {
+        itemName: "Nanotech Fabrics",
+        itemCategory: "Eco-Futurism Line",
+        itemLink: "https://www.youtube.com/acodegrid",
+        itemCopy: 
+            "The world of textiles has witnessed a revolution with the introduction of Advanced Nanotech Fabrics. \
+            Engineered on a micro-level, these fabrics promise to deliver both fashion and functionality in a single weave. \
+            Imagine a world where your dress naturally repels stains, or where your suit cleans itself. Beyond aesthetic appeal, \
+            it's the dawn of apparel that truly works for you.",
+        itemImg: "./img-1.jpg",
+    },
+    {
+        itemName: "Nanotech Fabrics",
+        itemCategory: "Eco-Futurism Line",
+        itemLink: "https://www.youtube.com/acodegrid",
+        itemCopy: 
+            "The world of textiles has witnessed a revolution with the introduction of Advanced Nanotech Fabrics. \
+            Engineered on a micro-level, these fabrics promise to deliver both fashion and functionality in a single weave. \
+            Imagine a world where your dress naturally repels stains, or where your suit cleans itself. Beyond aesthetic appeal, \
+            it's the dawn of apparel that truly works for you.",
+        itemImg: "./img-1.jpg",
+    },
+    {
+        itemName: "Nanotech Fabrics",
+        itemCategory: "Eco-Futurism Line",
+        itemLink: "https://www.youtube.com/acodegrid",
+        itemCopy: 
+            "The world of textiles has witnessed a revolution with the introduction of Advanced Nanotech Fabrics. \
+            Engineered on a micro-level, these fabrics promise to deliver both fashion and functionality in a single weave. \
+            Imagine a world where your dress naturally repels stains, or where your suit cleans itself. Beyond aesthetic appeal, \
+            it's the dawn of apparel that truly works for you.",
+        itemImg: "./img-1.jpg",
+    },
+    {
+        itemName: "Nanotech Fabrics",
+        itemCategory: "Eco-Futurism Line",
+        itemLink: "https://www.youtube.com/acodegrid",
+        itemCopy: 
+            "The world of textiles has witnessed a revolution with the introduction of Advanced Nanotech Fabrics. \
+            Engineered on a micro-level, these fabrics promise to deliver both fashion and functionality in a single weave. \
+            Imagine a world where your dress naturally repels stains, or where your suit cleans itself. Beyond aesthetic appeal, \
+            it's the dawn of apparel that truly works for you.",
+        itemImg: "./img-1.jpg",
+    },
+    {
+        itemName: "Nanotech Fabrics",
+        itemCategory: "Eco-Futurism Line",
+        itemLink: "https://www.youtube.com/acodegrid",
+        itemCopy: 
+            "The world of textiles has witnessed a revolution with the introduction of Advanced Nanotech Fabrics. \
+            Engineered on a micro-level, these fabrics promise to deliver both fashion and functionality in a single weave. \
+            Imagine a world where your dress naturally repels stains, or where your suit cleans itself. Beyond aesthetic appeal, \
+            it's the dawn of apparel that truly works for you.",
+        itemImg: "./img-1.jpg",
+    },
+    {
+        itemName: "Nanotech Fabrics",
+        itemCategory: "Eco-Futurism Line",
+        itemLink: "https://www.youtube.com/acodegrid",
+        itemCopy: 
+            "The world of textiles has witnessed a revolution with the introduction of Advanced Nanotech Fabrics. \
+            Engineered on a micro-level, these fabrics promise to deliver both fashion and functionality in a single weave. \
+            Imagine a world where your dress naturally repels stains, or where your suit cleans itself. Beyond aesthetic appeal, \
+            it's the dawn of apparel that truly works for you.",
+        itemImg: "./img-1.jpg",
+    },
+    {
+        itemName: "Nanotech Fabrics",
+        itemCategory: "Eco-Futurism Line",
+        itemLink: "https://www.youtube.com/acodegrid",
+        itemCopy: 
+            "The world of textiles has witnessed a revolution with the introduction of Advanced Nanotech Fabrics. \
+            Engineered on a micro-level, these fabrics promise to deliver both fashion and functionality in a single weave. \
+            Imagine a world where your dress naturally repels stains, or where your suit cleans itself. Beyond aesthetic appeal, \
+            it's the dawn of apparel that truly works for you.",
+        itemImg: "./img-1.jpg",
+    },
+    {
+        itemName: "Nanotech Fabrics",
+        itemCategory: "Eco-Futurism Line",
+        itemLink: "https://www.youtube.com/acodegrid",
+        itemCopy: 
+            "The world of textiles has witnessed a revolution with the introduction of Advanced Nanotech Fabrics. \
+            Engineered on a micro-level, these fabrics promise to deliver both fashion and functionality in a single weave. \
+            Imagine a world where your dress naturally repels stains, or where your suit cleans itself. Beyond aesthetic appeal, \
+            it's the dawn of apparel that truly works for you.",
+        itemImg: "./img-1.jpg",
+    },
+    {
+        itemName: "Nanotech Fabrics",
+        itemCategory: "Eco-Futurism Line",
+        itemLink: "https://www.youtube.com/acodegrid",
+        itemCopy: 
+            "The world of textiles has witnessed a revolution with the introduction of Advanced Nanotech Fabrics. \
+            Engineered on a micro-level, these fabrics promise to deliver both fashion and functionality in a single weave. \
+            Imagine a world where your dress naturally repels stains, or where your suit cleans itself. Beyond aesthetic appeal, \
+            it's the dawn of apparel that truly works for you.",
+        itemImg: "./img-1.jpg",
+    },
+    {
+        itemName: "Nanotech Fabrics",
+        itemCategory: "Eco-Futurism Line",
+        itemLink: "https://www.youtube.com/acodegrid",
+        itemCopy: 
+            "The world of textiles has witnessed a revolution with the introduction of Advanced Nanotech Fabrics. \
+            Engineered on a micro-level, these fabrics promise to deliver both fashion and functionality in a single weave. \
+            Imagine a world where your dress naturally repels stains, or where your suit cleans itself. Beyond aesthetic appeal, \
+            it's the dawn of apparel that truly works for you.",
+        itemImg: "./img-1.jpg",
+    },
+];
+export { data};
